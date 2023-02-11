596ae1dca2eee8274d8b2c3d2b12fe5a24c67b04
onurai <onurai@code.edu.az>
2023-02-11 16:57:14 +0400

    axios issue solved

--- src/Hooks/UseEffect.jsx
+++ src/Hooks/UseEffect.jsx
@@ -1,32 +1,68 @@
-import React from 'react'
+import axios from 'axios';
 import { useState, useEffect } from "react";
+import { ChakraProvider, Box, Spinner } from '@chakra-ui/react'
+// import { PuffLoader } from 'react-spinners/PuffLoader';
 
+// const override: CSSProperties = {
+//     display: "block",
+//     position: "absolute",
+//     top: "50%",
+//     left: "50%",
+//     transform: "translate(-50%)"
+//   };
 
 const UseEffect = () => {
-    const [count, setCount] = useState(0);
-    const [color, setColor] = useState('');
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
 
+
+    
     useEffect(() => {
-        clearInterval();
-    }, [color])
+        const getUsers  = async () => {
+            const response  = await axios.get('https://randomuser.me/api/?results=500')
+            console.log(response);
 
-    const IncreaseHandler = () =>{
-        setCount(count + 1);
-    }
+            setUsers(response.data.results);
+            setLoading(false);
+        }
+        
+        getUsers();
+    }, [])
 
-    const ColorHandler = () =>{
-        setColor('red');
-    }
-
-    setInterval(() => {
-        console.log('P413');
-    }, 1000)
+    
 
   return (
     <div>
         <p>Fetching data, Directly updating the DOM and Timers</p>
-        <button onClick={IncreaseHandler}>INCREASE</button>
-        <button onClick={ColorHandler}>ASSIGN THE COLOR</button>
+
+        {
+            loading ? 
+            <ChakraProvider>
+              <Box>
+              <Spinner
+                  thickness='4px'
+                  speed='0.65s'
+                  emptyColor='gray.200'
+                  color='blue.500'
+                  size='xl'
+                  position="absolute"
+                  top='300%'
+                  left='50%'
+                  transform='translate(-50%, -50%)'
+              />
+              </Box>
+            </ChakraProvider>
+            : users.map(user => {
+                return(
+                  <div key={user.id.value}>
+                    <p>{user.name.title}. {user.name.first} {user.name.last}</p>
+                    <p>{user.email}//{user.gender}//{user.location.city}, {user.location.city}</p>
+                    <p>{user.login.username}//{user.login.password}</p>
+                  </div>
+                )
+            })
+        }
+
     </div>
   )
 }
